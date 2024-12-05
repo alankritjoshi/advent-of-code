b884413288803500f03d74991a24d860b2f322fc
Alankrit Joshi <alankritjoshi@gmail.com>
2024-12-04 20:00:46 -0800

    2024/4 Part 2

--- 2024/4/main.py
+++ 2024/4/main.py
@@ -1,4 +1,5 @@
 import argparse
+import itertools
 
 def main() -> None:
     args = argparse.ArgumentParser(description="AoC runner")
@@ -20,35 +21,45 @@ def main() -> None:
 
             grid.append([ch for ch in line.split()[0]])
 
-    def find_xmas_counts(i: int, j: int, curr: int, direction: tuple[int, int]) -> int:
-        if grid[i][j] != 'XMAS'[curr]:
-            return 0
+    def find_xmas_counts(i: int, j: int, curr: int, direction: tuple[int, int], answer: list[tuple[int, int]]) -> list[tuple[int, int]]:
+        if grid[i][j] != 'MAS'[curr]:
+            return []
 
-        if curr == 3: # S in XMAS
-            return 1
+        if curr == 2: # S in XMAS
+            return answer
 
         ii, jj = i + direction[0], j + direction[1]
 
         if not (0 <= ii < len(grid) and 0 <= jj < len(grid[0])):
-            return 0
+            return []
 
-        return find_xmas_counts(ii, jj, curr + 1, direction)
+        return find_xmas_counts(ii, jj, curr + 1, direction, answer + [(ii, jj)])
 
-    total = 0
+    answers = []
 
+    # only find diagonals
     directions = [
-        (1, 0), (-1, 0), # vertical
-        (0, 1), (0, -1), # horizontal
         (1, 1), (-1, -1), # diagonal 1
         (1, -1), (-1, 1), # diagonal 2
     ]
+
     for i in range(len(grid)):
         for j in range(len(grid[0])):
-            if grid[i][j] == 'X':
+            # instead of XMAS, find MAS and retrieve their coordinates
+            if grid[i][j] == 'M':
                 for direction in directions:
-                    total += find_xmas_counts(i, j, 0, direction)
+                    answer = find_xmas_counts(i, j, 0, direction, [(i, j)])
+                    if answer:
+                        answers.append(answer)
 
-    print(total)
+    # find all pair combinations of MAS found
+    mas_pairs = 0
+    for mas_a, mas_b in itertools.combinations(answers, 2):
+        if mas_a[1] == mas_b[1]: # middle needs to be A to get a pair which makes an X
+            mas_pairs += 1
+
+    print(mas_pairs)
+
 
 if __name__ == '__main__':
     main()
