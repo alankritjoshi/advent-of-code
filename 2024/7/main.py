8da9fde26464649cde24b310f86241146e70a29c
Alankrit Joshi <alankritjoshi@gmail.com>
2024-12-06 23:52:22 -0800

    2024/7 Part 2

--- 2024/7/main.py
+++ 2024/7/main.py
@@ -1,4 +1,5 @@
 import argparse
+from enum import Enum, auto
 
 def main() -> None:
     args = argparse.ArgumentParser(description="AoC runner")
@@ -23,23 +24,36 @@ def main() -> None:
             vals = [int(val) for val in vals_str.strip().split()]
             tests.append((verify, vals))
 
-    def calc(index: int, so_far: int | None, verify: int, vals: list[int]) -> bool:
+    class Op(Enum):
+        PLUS = auto()
+        MULTI = auto()
+
+    def calc(index: int, so_far: int | None, last_op: Op | None, verify: int, vals: list[int]) -> bool:
         if index == len(vals):
             return so_far is not None and so_far == verify
 
         multi_so_far = so_far if so_far is not None else 1
-        if multi_so_far * vals[index] <= verify and calc(index+1, multi_so_far * vals[index], verify, vals):
+        if multi_so_far * vals[index] <= verify and calc(index+1, multi_so_far * vals[index], Op.MULTI, verify, vals):
             return True
 
         plus_so_far = so_far if so_far is not None else 0
-        if plus_so_far + vals[index] <= verify and calc(index+1, plus_so_far+ vals[index], verify, vals):
+        if plus_so_far + vals[index] <= verify and calc(index+1, plus_so_far + vals[index], Op.PLUS, verify, vals):
             return True
+
+        if index > 0:
+            assert last_op is not None
+            assert so_far is not None
+            curr = int(str(so_far) + str(vals[index]))
+            if last_op is Op.MULTI:
+                return calc(index+1, curr, last_op, verify, vals)
+            if last_op is Op.PLUS:
+                return calc(index+1, curr, last_op, verify, vals)
 
         return False
 
     total = 0
     for verify, vals in tests:
-        if calc(0, None, verify, vals):
+        if calc(0, None, None, verify, vals):
             total += verify
 
     print(total)
